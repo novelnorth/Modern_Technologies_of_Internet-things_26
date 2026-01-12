00fa28d29932ea4e6e3cbb7f2613c0c973fc1b02
Dmitry <167667342+novelnorth@users.noreply.github.com>
2026-01-12 17:26:09 +0500

    Создание config.py
    Скрипт-файл python с конфигурацией порогов и путям к файлам симуляции

---
+++ config.py
@@ -0,0 +1,8 @@
+# Пороги
+CRITICAL_LEVEL = 80  # Процент
+WARNING_LEVEL = 60   # Процент
+
+# Пути к файлам
+SENSOR_FILE = "sensors/oil_vapor_level.txt"
+TIMESTAMP_FILE = "sensors/timestamp.txt"
+LOG_FILE = "logs/alert.log"
